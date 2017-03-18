eb4f7c6015cf2d5973d90b2a6a9002a34fc3103e
Jorge Figueroa <jorge.figueroa@magmalabs.io>
2017-03-18 08:58:52 -0600

    Show social links on Coming Soon

--- src/client/assets/javascripts/features/ComingSoon/ComingSoon.jsx
+++ src/client/assets/javascripts/features/ComingSoon/ComingSoon.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'styles/components/ComingSoon.scss';
+import AwesomeLink from 'components/AwesomeLink';
 
 export default class ComingSoon extends React.Component {
   render() {
@@ -11,6 +12,13 @@ export default class ComingSoon extends React.Component {
           <p>
             Coming Soon. Visit <a href="https://yknx4.github.io/blog">my blog</a>.
           </p>
+          <span>
+            <AwesomeLink name="google" href="mailto:me@ale.world" />
+            <AwesomeLink name="facebook" href="//www.facebook.com/yknx4" />
+            <AwesomeLink name="github-alt" href="//www.github.com/yknx4" />
+            <AwesomeLink name="linkedin" href="//www.linkedin.com/in/yknx4/" />
+          </span>
+          <small>Ale Figueroa ❤︎</small>
         </section>
       </section>
     );
