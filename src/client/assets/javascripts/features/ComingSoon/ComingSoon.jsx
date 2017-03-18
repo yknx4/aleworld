fdf7abb13d55550da908fde6feec382383ee0387
Jorge Figueroa <jorge.figueroa@magmalabs.io>
2017-03-18 09:14:02 -0600

    Add normal email

--- src/client/assets/javascripts/features/ComingSoon/ComingSoon.jsx
+++ src/client/assets/javascripts/features/ComingSoon/ComingSoon.jsx
@@ -13,10 +13,11 @@ export default class ComingSoon extends React.Component {
             Coming Soon. Visit <a href="https://yknx4.github.io/blog">my blog</a>.
           </p>
           <span>
-            <AwesomeLink name="google" href="mailto:me@ale.world" />
             <AwesomeLink name="facebook" href="//www.facebook.com/yknx4" />
             <AwesomeLink name="github-alt" href="//www.github.com/yknx4" />
             <AwesomeLink name="linkedin" href="//www.linkedin.com/in/yknx4/" />
+            <AwesomeLink name="google" href="mailto:yknx.4.b@gmail.com" />
+            <AwesomeLink name="envelope" href="mailto:me@ale.world" />
           </span>
           <small>Ale Figueroa ❤︎</small>
         </section>
