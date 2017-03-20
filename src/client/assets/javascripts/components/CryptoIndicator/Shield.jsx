c208e047316354bc92efc43113ca9e2845b711f0
Jorge Figueroa <jorge.figueroa@magmalabs.io>
2017-03-20 02:54:31 -0600

    Badge with pure css

--- src/client/assets/javascripts/components/CryptoIndicator/Shield.jsx
+++ src/client/assets/javascripts/components/CryptoIndicator/Shield.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import 'styles/components/Shield.scss';
+import p from 'images/coins/BTC-alt.svg';
 
 export default function Shield (props) {
-  return <span className="shield">BTC</span>;
+  return (
+    <span>
+      <span className="shield-left">
+        BTC
+      </span>
+      <span className="shield-right">$1.1K</span>
+    </span>
+  );
 }
