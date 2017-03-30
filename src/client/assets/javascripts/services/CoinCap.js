df4615584d60992f45f038d73fbc3e0fc602b5d2
Ale Figueroa <alejandro@progressly.com>
2017-03-30 13:19:16 -0600

    Last try to fix CoinCap over secure network

--- src/client/assets/javascripts/services/CoinCap.js
+++ src/client/assets/javascripts/services/CoinCap.js
@@ -27,7 +27,7 @@ class CoinCap {
 
   init() {
     info('Initializing CoinCap.');
-    this.socket = connect('//socket.coincap.io', {secure: true});
+    this.socket = connect('//coincap.io', {secure: true});
     each(EventTypes, this.bindEvent.bind(this));
     this.addTradeListener(this.checkCoin.bind(this));
   }
