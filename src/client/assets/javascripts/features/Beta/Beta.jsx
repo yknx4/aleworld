1dc1bff1e9cf26021f786c8082727b2f9756e544
Ale Figueroa <alejandro@progressly.com>
2017-04-02 15:55:03 -0500

    Move info tiles to Component

--- src/client/assets/javascripts/features/Beta/Beta.jsx
+++ src/client/assets/javascripts/features/Beta/Beta.jsx
@@ -17,6 +17,46 @@ import {
 import Header from './Header';
 import Title from './Title';
 import Footer from './Footer';
+import InfoTile from './InfoTile';
+
+const tiles = [
+  {
+    id: 'itile1',
+    picture: {
+      alt: 'Ale Figueroa',
+      src: ale2,
+    },
+    title: 'Ale Figueroa',
+    subtitle: 'Android Developer',
+    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.'
+  },
+  {
+    id: 'itile2',
+    picture: {
+      alt: 'Ale Figueroa',
+      src: ale3,
+    },
+    title: 'Ale Figueroa',
+    subtitle: 'Social Rebel Transgirl',
+    description: `I’m a supporter of Social Freedom, Internet Rights and the LGBT Community. I have the opinion that everyone can and shall live however the f*ck they want as long as they don’t harm anyone else’s freedom.
+    <br/>
+    I think any society should be free of oppression, and have all their human rights assured regardless of their political, religious, sexual, or whatever position.
+    <br/>
+    I think all those rights should be also preserved and enforced in all the modern environments that are constantly surging in the Internet (Yep, This means that you should also be able to do whatever you want on the Internet).
+    <br/>
+    And I’m against Gender Roles (a byproduct of sexism), I don’t think a person can be defined by what society tells them they are supposed to be just because the person was born with or without something between the legs. I consider myself transgender, but feel free to address me however you feel you should.`
+  },
+  {
+    id: 'itile3',
+    picture: {
+      alt: 'Ale Figueroa',
+      src: ale,
+    },
+    title: 'Ale Figueroa',
+    subtitle: 'Fullstack Developer',
+    description: 'Web is the future.'
+  },
+];
 
 export default class Beta extends Component {
 
@@ -29,49 +69,7 @@ export default class Beta extends Component {
               <section>
                 <Grid>
                   <Row>
-                    <Col md={4} sm={6}>
-                      <div className="image-tile outer-title text-justify">
-                        <img alt="Ale Figueroa" src={ale2} className="img-circle sm-img"/>
-                        <div className="title mb16">
-                          <h5 className="uppercase mb0">Ale Figueroa</h5>
-                          <span>Android Developer</span>
-                        </div>
-                        <p className="mb0">
-                          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.
-                        </p>
-                      </div>
-                    </Col>
-                    <Col md={4} sm={6}>
-                      <div className="image-tile outer-title text-justify">
-                        <img alt="Ale Figueroa" src={ale3} className="img-circle sm-img"/>
-                        <div className="title mb16">
-                          <h5 className="uppercase mb0">Ale Figueroa</h5>
-                          <span>Social Rebel Transgirl</span>
-                        </div>
-                        <p className="mb0">
-                          I’m a supporter of Social Freedom, Internet Rights and the LGBT Community. I have the opinion that everyone can and shall live however the f*ck they want as long as they don’t harm anyone else’s freedom.
-                          <br/>
-                          I think any society should be free of oppression, and have all their human rights assured regardless of their political, religious, sexual, or whatever position.
-                          <br/>
-                          I think all those rights should be also preserved and enforced in all the modern environments that are constantly surging in the Internet (Yep, This means that you should also be able to do whatever you want on the Internet).
-                          <br/>
-                          And I’m against Gender Roles (a byproduct of sexism), I don’t think a person can be defined by what society tells them they are supposed to be just because the person was born with or without something between the legs. I consider myself transgender, but feel free to address me however you feel you should.
-                        </p>
-                      </div>
-                    </Col>
-                    <Col md={4} sm={6}>
-                      <div className="image-tile outer-title text-justify">
-                        <img alt="Ale Figueroa" src={ale} className="img-circle sm-img"/>
-                        <div className="title mb16">
-                          <h5 className="uppercase mb0">Ale Figueroa</h5>
-                          <span>Full Stack Developer</span>
-                        </div>
-                        <p className="mb0">
-                          Web is the future. <br />
-
-                        </p>
-                      </div>
-                    </Col>
+                    {tiles.map((tile) => <Col key={tile.id} md={4} sm={6}><InfoTile {...tile}/></Col>)}
                   </Row>
                 </Grid>
               </section>
