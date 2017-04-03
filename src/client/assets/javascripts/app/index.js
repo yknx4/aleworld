ef0243a876f498241a39d8ac24a5b9c99cdd3bf7
Ale Figueroa <alejandro@progressly.com>
2017-04-02 19:51:48 -0500

    Fix React Router

--- src/client/assets/javascripts/app/index.js
+++ src/client/assets/javascripts/app/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
 import Redbox from 'redbox-react';
 
@@ -9,7 +7,6 @@ import Root from './Root';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
 
 // Get the DOM Element that will host our React application
 const rootEl = document.getElementById('app');
